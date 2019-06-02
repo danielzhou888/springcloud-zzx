11a3ef64e95b1b95e212c42f3103a5ca68c467ce
Daniel Zhou <danielzhoulove@gmail.com>
2019-06-02 23:02:13 +0800

    Create a spring annotation project

---
+++ zzx-spring-annotation/src/main/java/com/zzx/spring/annotation/ZzxSpringAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.zzx.spring.annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzxSpringAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzxSpringAnnotationApplication.class, args);
+    }
+
+}
