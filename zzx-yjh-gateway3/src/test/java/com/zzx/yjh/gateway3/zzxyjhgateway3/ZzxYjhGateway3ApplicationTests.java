7fffba71c292656e481a1ed76550d7b50b4e3134
zhouzhixiang <zhouzhixiang@hooyue.com>
2019-08-12 21:51:26 +0800

    3

---
+++ zzx-yjh-gateway3/src/test/java/com/zzx/yjh/gateway3/zzxyjhgateway3/ZzxYjhGateway3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzx.yjh.gateway3.zzxyjhgateway3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZzxYjhGateway3ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
