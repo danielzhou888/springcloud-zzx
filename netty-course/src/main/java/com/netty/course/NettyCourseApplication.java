0698ec0526f6e0d556dab808c546cda78a4d626b
zhouzhixiang <zhouzhixiang@ddky.com>
2022-07-29 13:43:14 +0800

    netty course record

---
+++ netty-course/src/main/java/com/netty/course/NettyCourseApplication.java
@@ -0,0 +1,13 @@
+package com.netty.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyCourseApplication.class, args);
+    }
+
+}
