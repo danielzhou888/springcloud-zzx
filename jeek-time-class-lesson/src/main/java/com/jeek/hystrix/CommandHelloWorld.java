b60f4055b24c39fcd47acd349a2f4e06a8dc1145
zhouzhixiang <zhouzhixiang@ddky.com>
2024-05-28 22:05:33 +0800

    极客时间

--- jeek-time-class-lesson/src/main/java/com/jeek/hystrix/CommandHelloWorld.java
+++ jeek-time-class-lesson/src/main/java/com/jeek/hystrix/CommandHelloWorld.java
@@ -1,4 +1,33 @@
 package com.jeek.hystrix;
 
-public class CommandHelloWorld {
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixThreadPoolKey;
+
+public class CommandHelloWorld extends HystrixCommand<String> {
+
+    protected CommandHelloWorld(HystrixCommandGroupKey group) {
+        super(group);
+    }
+
+    protected CommandHelloWorld(HystrixCommandGroupKey group, HystrixThreadPoolKey threadPool) {
+        super(group, threadPool);
+    }
+
+    protected CommandHelloWorld(HystrixCommandGroupKey group, int executionIsolationThreadTimeoutInMilliseconds) {
+        super(group, executionIsolationThreadTimeoutInMilliseconds);
+    }
+
+    protected CommandHelloWorld(HystrixCommandGroupKey group, HystrixThreadPoolKey threadPool, int executionIsolationThreadTimeoutInMilliseconds) {
+        super(group, threadPool, executionIsolationThreadTimeoutInMilliseconds);
+    }
+
+    protected CommandHelloWorld(Setter setter) {
+        super(setter);
+    }
+
+    @Override
+    protected String run() throws Exception {
+        return null;
+    }
 }
